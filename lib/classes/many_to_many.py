243b9d8529e079c6e5fc8056bb967eb747b8d5c6
Lord_Smiley_Face <oowuoroowuor@gmail.com>
2024-06-13 15:18:13 +0300

    submit concerts code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,41 +1,92 @@
 class Band:
+    all = []
+    
     def __init__(self, name, hometown):
-        self.name = name
-        self.hometown = hometown
+        self._name = name
+        self._hometown = hometown
+        Band.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+
+    @property
+    def hometown(self):
+        return self._hometown
+    
+    @hometown.setter
+    def hometown(self, value):
+        raise AttributeError("Can't set attribute")
 
     def concerts(self):
-        pass
-
+        return [c for c in Concert.all if c.band == self]
+    
     def venues(self):
-        pass
+        return list(set([concert.venue for concert in self.concerts()]))
 
     def play_in_venue(self, venue, date):
-        pass
-
+        return Concert(date, self, venue)
+    
     def all_introductions(self):
-        pass
-
+        return list(concert.introduction() for concert in self.concerts())
 
 class Concert:
+    all = []
+    
     def __init__(self, date, band, venue):
-        self.date = date
+        self._date = date
         self.band = band
         self.venue = venue
+        Concert.all.append(self)
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._date = value
 
     def hometown_show(self):
-        pass
+        return self.band.hometown == self.venue.city
 
     def introduction(self):
-        pass
-
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
 
 class Venue:
+    all = []
+    
     def __init__(self, name, city):
-        self.name = name
-        self.city = city
+        self._name = name
+        self._city = city
+        Venue.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+
+    @property
+    def city(self):
+        return self._city
+    
+    @city.setter
+    def city(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._city = value
 
     def concerts(self):
-        pass
+        return [c for c in Concert.all if c.venue == self]
 
     def bands(self):
-        pass
+        return list(set([concert.band for concert in self.concerts()]))
